56a7cde33860453e94c0f41c68c95583d281eb52
Biplav Adhikary <biplov@live.com>
2021-10-29 01:48:08 +0530

    Mock Api Initial Setup

---
+++ app.js
@@ -0,0 +1,51 @@
+var createError = require("http-errors");
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/indexRoute");
+var authenticateUserRouter = require("./routes/authenticateUser");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+/**
+ * START: Main Routes
+ */
+
+app.use("/", indexRouter);
+app.use("/authenticateUser", authenticateUserRouter);
+
+/**
+ * END: Main Routes
+ */
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ routes/authenticateUser/index.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var router = express.Router();
+var data = require("./index.json");
+
+/* GET users listing. */
+router.get("/", function (req, res) {
+  const { query: { userName, password } = {} } = req;
+
+  const userData = data.find((userData) => userData.userName === userName);
+
+  const sendDelayedResponse = (callback) => {
+    setTimeout(callback, 3000);
+  };
+
+  if (!userName || !password) {
+    sendDelayedResponse(() =>
+      res.send({
+        success: false,
+        message: "Invalid Request Data",
+        data: null,
+      })
+    );
+    return;
+  }
+
+  if (!userData) {
+    sendDelayedResponse(() =>
+      res.send({
+        success: false,
+        message: "Could not find the required user",
+        data: null,
+      })
+    );
+  } else {
+    if (userData.password !== password) {
+      sendDelayedResponse(() =>
+        res.send({ success: false, message: "Invalid Credentials", data: null })
+      );
+    } else {
+      success = true;
+      sendDelayedResponse(() =>
+        res.send({ success: true, message: "Sending Data...", data: userData })
+      );
+    }
+  }
+});
+
+module.exports = router;
