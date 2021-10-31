9850a762084af23b1e182efc8732506d3148c106
Biplav Adhikary <biplov@live.com>
2021-10-31 18:04:29 +0530

    Added logs

--- socketEvents/sendMessage/index.js
+++ socketEvents/sendMessage/index.js
@@ -2,6 +2,8 @@ const eventNames = require("../eventNames");
 
 const sendMessage = (id, socket) => {
   const sendMessageHandler = ({ recipients, messageItem }) => {
+    console.log("Received Data", { recipients, messageItem });
+
     recipients.forEach((recipient) => {
       const otherReceipients = recipients.filter((r) => r !== recipient);
       socket.broadcast.to(recipient).emit(eventNames.RECEIVE_MESSAGE, {
